c658060ea90c830d0178f7f8218b1ddd1d895688
Shailee <shailee.madhani@gmail.com>
2021-06-11 18:21:16 -0700

    Final minus 1

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -54,3 +54,29 @@ from current_emp as ce
 left join dept_employees as de on ce.emp_no=de.emp_no
 group by de.dept_no
 order by de.dept_no 
+
+--emp_info
+SELECT e.first_name,e.last_name,e.gender,e.emp_no,s.salary,de.to_date
+INTO emp_info
+FROM employees as e
+inner join salaries as s
+on e.emp_no=s.emp_no
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31') AND (de.to_date = '9999-01-01');
+
+--manager_info
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
